5419df64580a71593fcef57bfc514d7d6820a852
202108 <202108@WINMAC14>
2025-02-20 15:10:30 +0300

    Добавьте файлы проекта.

---
+++ Lapushok/DB/Partials/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapushok.DB
+{
+    public partial class Product
+    {
+        public string MaterialsList
+        {
+            get
+            {
+                return $"Материалы: {string.Join(" ,", ProductMaterial.Select(x => x.Material.Name))}";
+            }
+        }
+
+        public string Photo
+        {
+            get
+            {
+                string result = Image;
+                string imagePng = "/Images/picture.png";
+
+                if (result == null)
+                {
+                    return imagePng;
+                }
+                else
+                {
+                    return result;
+                }
+
+            }
+        }
+        
+        //public string ColorCard
+        //{
+        //    get 
+        //    {
+        //        string result = "#ff4c5b";
+        //        if (DateTime.mon)
+        //        {
+        //            return
+        //        }
+        //        else
+        //        {
+        //            return 
+        //        }
+        //    }
+        //}
+    }
+}
