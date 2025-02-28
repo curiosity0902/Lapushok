353a5ef3af0b0356e12f5dff2890b0531b5ce3f1
202108 <202108@WINMAC14>
2025-02-28 16:47:49 +0300

    -- changes

--- Lapushok/Windoww/AddProductWindow.xaml.cs
+++ Lapushok/Windoww/AddProductWindow.xaml.cs
@@ -22,19 +22,35 @@ namespace Lapushok.Windoww
     {
         public static List<Product> products { get; set; }
         public static List<Material> materials { get; set; }
+        public static List<ProductMaterial> productMaterials { get; set; }
         public AddProductWindow()
         {
             InitializeComponent();
 
             products = new List<Product>(DBConnection.lapushokEntities.Product.ToList());
             materials = new List<Material>(DBConnection.lapushokEntities.Material.ToList());
+            productMaterials = new List<ProductMaterial>(DBConnection.lapushokEntities.ProductMaterial.ToList());
 
             MaterialsLv.ItemsSource = materials;
         }
 
         private void MaterialCbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (MaterialsLv.SelectedItem is Material selectedMaterial)
+            {
+                    var newMedicineShedule = new ProductMaterial
+                    {
+                        IDShedule = contextShedule.IDShedule,
+                        IDMedicine = selectedMedicine.IDMedicine
+                    };
 
+                    DBConnection.lapushokEntities.MedicineShedule.Add(newMedicineShedule);
+                    DBConnection.lapushokEntities.SaveChanges();
+
+                    medicineShedules.Add(newMedicineShedule);
+                    MaterialsLv.ItemsSource = null;
+                    MaterialsLv.ItemsSource = medicineShedules.Select(ms => ms.Medicine).ToList();
+            }
         }
     }
 }
